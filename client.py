327af913c3c6c85443aec5c72788892f9281cfd8
Maciej Fularz <fularzmaciej@gmail.com>
2023-04-24 22:04:59 +0200

    First version of client and server side

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket
+import json
+
+
+class Client:
+    def __init__(self, host, port):
+        self.address = (host, port)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect(self.address)
+
+    def send_receive(self):
+        print("""
+Commands availible:
+- help
+- info
+- uptime
+- stop """)
+        while True:
+            user_command = input('Enter the command!: ')
+            self.socket.sendall(json.dumps({'command': user_command}).encode('utf8'))
+            return_msg = self.socket.recv(1024).decode('utf-8')
+            return_msg = json.loads(return_msg)
+            if return_msg['message'] == 'stop':
+                print('Server is shutting down!')
+                break
+            else:
+                for key, desc in return_msg['message'].items():
+                    print(f'{key} - {desc}')
+                
+                
+if __name__ == '__main__':
+    client = Client('127.0.0.1', 64321)
+    client.send_receive()
---
+++ server.py
@@ -0,0 +1,70 @@
+import socket
+import json
+import datetime
+
+
+class Server:
+    def __init__(self, host, port):
+        self.address = (host, port)
+        self.server_start_time = datetime.datetime.now()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(self.address)
+        self.socket.listen()
+        print(f"Listening on {self.address}")
+
+    def available_commands(self):
+        commands = {
+            'uptime': "returns the uptime of the server",
+            'info': "returns version of server and it's date creation",
+            'help': "show list of commands",
+            'stop': "stops server and client",
+        }
+        return {'message': commands}
+
+    def uptime(self):
+        uptime = {"uptime": str(datetime.datetime.now() - self.server_start_time)}
+        return {'message': uptime}
+
+    @property
+    def info(self):
+        info = {
+            'version': '1.0',
+            'creation_date': str(self.server_start_time),
+        }
+        return {'message': info}
+
+    def run(self):
+        conn, addr = self.socket.accept()
+        print(f'Connected by {addr}')
+        while True:
+            msg = conn.recv(1024).decode('utf8')
+            if not msg:
+                break
+            
+            msg = json.loads(msg)
+            command = msg['command']
+            print(f'Client send command - {command}')
+            if command == 'uptime':
+                conn.send(json.dumps(self.uptime()).encode('utf8'))
+                print(f'Sending uptime to client ')
+
+            elif command == 'info':
+                conn.send(json.dumps(self.info).encode('utf8'))
+                print(f'Sending server info to client ')
+
+            elif command == 'help':
+                conn.send(json.dumps(self.available_commands()).encode('utf8'))
+                print(f'Sending command list to client ')
+
+            elif command == 'stop':
+                conn.send(json.dumps({'message': 'stop'}).encode('utf8'))
+                print(f'Shutting down a server')
+                self.socket.close()
+                break
+            else:
+                conn.send(json.dumps({'message': 'Unknown command'}).encode('utf8'))
+
+
+if __name__ == '__main__':
+    server = Server('127.0.0.1', 64321)
+    server.run()
