650d1a5a4c21a3aa585d72a922537ecf1f62d8ec
Maciej Fularz <fularzmaciej@gmail.com>
2023-05-24 21:15:40 +0200

    First steps into adding postgresql to project

---
+++ database/database_connection.py
@@ -0,0 +1,21 @@
+import psycopg2
+from dotenv import dotenv_values
+
+
+
+class DatabaseConnection:
+    def __init__(self, host: str):
+        self.connection = None
+        self.host = host
+
+    def __enter__(self):
+        self.connection = psycopg2.connect(self.host)
+        return self.connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.commit()
+        self.connection.close()
+
+
+
+db = DatabaseConnection(dotenv_values('.env'))
