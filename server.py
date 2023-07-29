e810ab274ca8e173c25f4b62ab13675061701ff3
Maciej Fularz <fularzmaciej@gmail.com>
2023-07-29 22:38:20 +0200

    Rewrite tests

--- server.py
+++ server.py
@@ -17,46 +17,63 @@ class ClientThread(threading.Thread):
     def run(self):
         print(f'Connected by {self.address}')
 
-        while True:
-            msg = self.conn.recv(1024).decode('utf8')
-            if not msg:
-                break
+        try:
+            while True:
+                msg = self.conn.recv(1024).decode('utf8')
+                if not msg:
+                    break
 
-            msg = json.loads(msg)
-            command = msg['command'].split()
+                msg = json.loads(msg)
+                command = msg['command'].split()
 
-            if command and command[0] == 'stop':
-                self.conn.send(json.dumps({'message': 'stop'}).encode('utf-8'))
-                print(f'Shutting down a server')
-                self.conn.close()
-                break
-            else:
-                self.conn.send(
-                    json.dumps(
-                        self.handler.handle_commands(command),
-                        indent=4).encode('utf-8'))
+                if command and command[0] == 'stop':
+                    self.conn.send(json.dumps({'message': 'stop'}).encode('utf-8'))
+                    print(f'Shutting down a server')
+                    break
+                else:
+                    self.conn.send(
+                        json.dumps(
+                            self.handler.handle_commands(command),
+                            indent=4).encode('utf-8'))
+        except Exception as e:
+            print(f"Exception in client thread: {e}")
+        finally:
+            self.conn.close()
 
 
 class Server:
-    def __init__(self, host, port):
+    def __init__(self, host, port, database='database.env'):
         self.address = (host, port)
         self.server_start_time = datetime.datetime.now()
+
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(self.address)
         self.socket.listen()
+
         print(f"Listening on {self.address}")
-        self.db = ClientServerDatabase('database.env')
+        self.db = ClientServerDatabase(database)
         self.handler = ServerResponseHandler(self.server_start_time)
+        self.client_threads = []  # List to store active client threads
+        self.server_run = True
 
     def run(self):
         # Accept incoming connections
-        while True:
+        while self.server_run:
             conn, addr = self.socket.accept()
             print(conn, addr)
 
             # Create new thread for each client
             client_thread = ClientThread(conn, addr, self.handler)
+            self.client_threads.append(client_thread)
             client_thread.start()
+
+    def stop(self):
+        # Close all client threads gracefully
+        self.server_run = False
+        for thread in self.client_threads:
+            thread.join()  # Wait for each thread to finish
+        self.socket.close()
+        self.db.close_connections()
 
 
 if __name__ == '__main__':
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,30 +1,55 @@
 import unittest
 import socket
-
+import json
+import threading
+from random import choice
 
 from server import Server
-from users.users import User
 
 
 class TestServer(unittest.TestCase):
     def setUp(self) -> None:
         self.host = '127.0.0.1'
-        self.port = 64322
-        
-        # Initialize the Server instance
-        self.server = Server(self.host, self.port)
+        self.ports = range(64322, 64390)
+        self.port = choice(self.ports)
 
-        # Create a client socket and connect to the server
+        # Initialize the Server instance and event to signal when server is ready
+        self.server = Server(self.host, self.port, 'test_database.env')
+        self.server_ready_event = threading.Event()
+
+        # Create a client socket and event to signal when client is ready
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect((self.host, self.port))
-        
-        # Initialize the User instance
-        self.user = User()
-    
+        self.client_ready_event = threading.Event()
+
+        self.server_thread = None
+        self.client_thread = None
+
+        self.start_server()
+        self.start_client()
+
+        self.client_ready_event.wait()
+
     def tearDown(self):
         # Close the server socket and the client socket
-        self.server.socket.close()
         self.client.close()
+        self.server.stop()
+
+    def start_server(self):
+        def run_server():
+            self.server.run()
+
+        self.server_thread = threading.Thread(target=run_server)
+        self.server_thread.start()
+        self.server_ready_event.set()
+
+    def start_client(self):
+        def run_client():
+            self.server_ready_event.wait()
+            self.client.connect((self.host, self.port))
+            self.client_ready_event.set()
+
+        self.client_thread = threading.Thread(target=run_client)
+        self.client_thread.start()
 
     def test_available_commands_before_login(self):
         # Test the available_commands_before_login method
@@ -36,11 +61,13 @@ class TestServer(unittest.TestCase):
             'register': 'registers new users. usage: <register username password>',
             'login': 'log in user. usage: <login username password>'
         }}
-        self.assertEqual(self.server.handler.available_commands_before_login(), expected_return)
+        self.client.send(json.dumps({'command': 'help'}).encode('utf-8'))
+        data = self.client.recv(1024).decode('utf-8')
+        self.assertEqual(json.loads(data), expected_return)
 
     def test_available_commands_after_login(self):
         # Test the available_commands_after_login method
-        self.server.user.username = 'test_user'
+        self.server.handler.user.username = 'test_user'
         expected_return = {'message': {
             'uptime': "returns the uptime of the server",
             'info': "returns version of server and it's date creation",
@@ -51,20 +78,26 @@ class TestServer(unittest.TestCase):
             'send': 'send user a message. usage <send user message(max. 255 chars)>',
             'unread': 'shows unread messages from users inbox. usage <unread username>',
             'inbox': 'shows users inbox. usage: <inbox username>',
-            'user_list': 'shows users in the server'  
+            'user_list': 'shows users in the server'
         }}
-        self.assertEqual(self.server.handler.available_commands_after_login(), expected_return)
-        
+        self.client.send(json.dumps({'command': 'help'}).encode('utf-8'))
+        data = self.client.recv(1024).decode('utf-8')
+        self.assertEqual(json.loads(data), expected_return)
+
     def test_uptime(self):
         # Test the uptime property
-        uptime_str = self.server.handler.uptime['message']['uptime']
-        self.assertRegex(uptime_str, r'^[0-9]+:[0-9]+:[0-9]+\.[0-9]{6}$')
+        self.client.send(json.dumps({'command': 'uptime'}).encode('utf-8'))
+        data = json.loads(self.client.recv(1024).decode('utf-8'))
+        data = data['message']
+        self.assertRegex(data['uptime'], r'^[0-9]+:[0-9]+:[0-9]+\.[0-9]{6}$')
 
     def test_info(self):
         # Test the info property
-        info_dict = self.server.handler.info['message']
-        self.assertRegex(info_dict['Version'], r'^[0-9]+.[0-9]+.[0-9]+$')
-        self.assertEqual(info_dict['Creation_date'], str(self.server.server_start_time))
+        self.client.send(json.dumps({'command': 'info'}).encode('utf-8'))
+        data = json.loads(self.client.recv(1024).decode('utf-8'))
+        data = data['message']
+        self.assertRegex(data['Version'], r'^[0-9]+.[0-9]+.[0-9]+$')
+        self.assertEqual(data['Creation_date'], str(self.server.server_start_time))
 
 
 if __name__ == '__main__':
